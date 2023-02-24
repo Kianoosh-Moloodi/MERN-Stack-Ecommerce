33f42e2021d8191d7677b50c1c4635d2551f62f7
Kianoosh Moloodi <kianoosh.moloodi@gmail.com>
2023-02-24 15:34:15 +0100

    creating ProductCard and ProductsScreen components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ProductsScreen from './screens/ProductsScreen';
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Navbar />
+        <main>
+          <Routes>
+            <Route path='/products' element={<ProductsScreen />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </ChakraProvider>
   );
